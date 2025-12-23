d751eb584e727cf285b1c707c49e7e25d75a6bdd
duyidao <2014567296@qq.com>
2025-12-23 18:11:48 +0800

    feat: 预加载初步添加

--- src/views/vue/preloadImage/index.ts
+++ src/views/vue/preloadImage/index.ts
@@ -3,16 +3,31 @@
  * @params images 图片资源数组
  * @returns Promise
  */
-export function preloadImages(images) {
-  return new Promise((resolve, reject) => {
-    images.forEach((image) => {
+export function preloadImages(images, max = 3) {
+  const _images = [...images];
+  
+  function load() {
+    const src= _images.shift()
+    return new Promise((resolve, reject) => {
       const link = document.createElement('link');
       link.rel = 'preload';
       link.as = 'image';
-      link.href = image;
+      link.href = src;
       document.head.appendChild(link);
-    });
-    images.onload = resolve;
-    images.onerror = reject;
-  })
+      link.onload = resolve;
+      link.onerror = reject;
+    })
+  }
+
+  function _load() {
+    load().finally(() => {
+      if (_images.length) {
+        _load();
+      }
+    })
+  }
+
+  for (let i = 0; i < max; i++) {
+    _load();
+  }
 }
--- src/views/vue/preloadImage/index.vue
+++ src/views/vue/preloadImage/index.vue
@@ -2,16 +2,16 @@
 import { preloadImages } from './index'
 
 const images = [
-  'https://picsum.photos/200/100?random=1',
-  'https://picsum.photos/200/100?random=2',
-  'https://picsum.photos/200/100?random=3',
-  'https://picsum.photos/200/100?random=4',
-  'https://picsum.photos/200/100?random=5',
-  'https://picsum.photos/200/100?random=6',
-  'https://picsum.photos/200/100?random=7',
-  'https://picsum.photos/200/100?random=8',
-  'https://picsum.photos/200/100?random=9',
-  'https://picsum.photos/200/100?random=10',
+  'https://picsum.photos/2000/1000?random=1',
+  'https://picsum.photos/2000/1000?random=2',
+  'https://picsum.photos/2000/1000?random=3',
+  'https://picsum.photos/2000/1000?random=4',
+  'https://picsum.photos/2000/1000?random=5',
+  'https://picsum.photos/2000/1000?random=6',
+  'https://picsum.photos/2000/1000?random=7',
+  'https://picsum.photos/2000/1000?random=8',
+  'https://picsum.photos/2000/1000?random=9',
+  'https://picsum.photos/2000/1000?random=10',
 ];
 
 preloadImages(images);
@@ -19,7 +19,7 @@ preloadImages(images);
 
 <template>
   <div>
-    <img v-for="image in images" :key="image" :src="image" />
+    <img v-for="image in images" :key="image" :src="image" class="w-200 h-100" />
   </div>
 </template>
 
